b7042daa1a4f8352a0e7b641804435e91bef1365
younghwani <younghwankim624@gmail.com>
2022-03-05 00:05:57 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by kimyounghwan on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
